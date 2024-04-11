7262c2cf04afc82d1d900f2deac87dbf791aeeec
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-11 14:32:08 +0300

    finalize on the code

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,23 @@ public class Main {
         System.out.println(empIds);
         System.out.println(empIds.get("Carl"));
         System.out.println(empIds.containsKey("Carl"));
+        System.out.println(empIds.containsValue(86942));
+
+        empIds.put("Edwin", 78653);
+        System.out.println(empIds);
+        System.out.println("----------------------------------------------------------------");
+
+//        empIds.replace("Kramer, 777");
+        System.out.println(empIds);
+
+        empIds.putIfAbsent("Steve", 787 );
+        System.out.println(empIds);
+        System.out.println("----------------------------------------------------------------");
+
+        empIds.remove("Steve");
+        System.out.println(empIds);
+
+        System.out.println("----------------------------------------------------------------");
 
     }
 }
